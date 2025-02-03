35373d2d4a1f644ccef5ce087555ab175af860d3
Aadii-01 <blazehere95@gmail.com>
2025-02-03 20:02:39 +0530

    JS Dom

---
+++ dom.js
@@ -0,0 +1,13 @@
+// let mydiv = document.querySelector("#fdiv");
+// let newElement = document.createElement('span');
+// newElement.textContent = "Aadi";
+
+// mydiv.insertAdjacentElement('beforebegin',newElement);
+
+// let parent= document.querySelector("#fdiv");
+// let child = document.querySelector("fpara");
+// parent.removeChild(child);
+
+
+let paraElement = document.getElementById("spara");
+paraElement.setAttribute("style","padding:10rem");
